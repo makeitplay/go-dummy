a261dc719010ea1a224cbca0db897dfeee55686c
Rubens Silva <rubens21@gmail.com>
2019-03-15 14:47:56 -0400

    implementing the ball holder decision maker

--- dummie/decision_holding.go
+++ dummie/decision_holding.go
@@ -1,62 +1,162 @@
 package dummie
 
 import (
+	"github.com/makeitplay/arena"
 	"github.com/makeitplay/arena/orders"
-	"github.com/makeitplay/the-dummies-go/strategy"
 	"github.com/makeitplay/arena/physics"
 	"github.com/makeitplay/arena/units"
-	"math"
 	"github.com/makeitplay/client-player-go"
+	"github.com/makeitplay/the-dummies-go/strategy"
+	"math"
 )
 
-
-func (d *Dummie) orderForHoldingTheBall() (msg string, orders []orders.Order) {
+// Shoot/Pass		MustNot			shouldNot		may			Should			Must
+// MustNot			Advance			Advance			Advance		Pass			Pass
+// ShouldNot		Advance			Advance			Advance		Pass			Pass
+// May				*				*				*			Pass			Pass
+// Should			Shoot			Shoot			Shoot		Shoot			Shoot
+// Must				Shoot			Shoot			Shoot		Shoot			Shoot
+func (d *Dummie) orderForHoldingTheBall() (msg string, ordersSet []orders.Order) {
 	player := d.Player
 
-	if d.InShootZone() {
-		goForward := player.OpponentGoal().Center
-		vecToGoal, err := physics.NewVector(player.Coords, goForward)
-		if err != nil {
-			d.Logger.Errorf("could not find the vector to the goal: %s", err)
-			return "I have a issue, somebody else hold the ball", d.orderPassTheBall()
-		}
-		vecToGoal.SetY(0) //we want the player go straight
+	shouldIShoot, target := ShouldShoot(player, d.GameMsg)
+	if shouldIShoot >= Should {
+		order, _ := d.Player.CreateKickOrder(d.GameMsg.Ball(), *target, units.BallMaxSpeed)
+		return "Shoot!", []orders.Order{order}
+	}
 
-		obstaclesOnPath, err := strategy.WatchOpponentOnMyRoute(player.Coords, vecToGoal.TargetFrom(player.Coords), units.PlayerSize, player.GetOpponentTeam(d.GameMsg.GameInfo))
-		if len(obstaclesOnPath) > 0 {
-			return "I am free yet", []orders.Order{d.orderAdvance()}
+	orderToAdvance, _ := d.Player.CreateMoveOrderMaxSpeed(physics.Point{
+		PosX: player.OpponentGoal().Center.PosX,
+		PosY: player.Coords.PosY,
+	})
+
+	shouldIPass, candidatePlayers := fuzzyDecisionPass(player, d.GameMsg)
+	if shouldIPass >= Should {
+		bastCandidate := electBestCandidate(candidatePlayers)
+		order, _ := d.Player.CreateKickOrder(d.GameMsg.Ball(), bastCandidate.Coords, units.BallMaxSpeed)
+		return "Found a well positioned mate", []orders.Order{order}
+	}
+
+	if shouldIShoot <= ShouldNot {
+		return "No good options, keeping moving", []orders.Order{orderToAdvance}
+	}
+
+	//from this point we know we should not pass, and we may shoot but it is probably a bad idea
+	return "do not stop to swimming", []orders.Order{orderToAdvance}
+
+	return
+}
+
+func ShouldShoot(player *client.Player, gameMsg *client.GameMessage) (FuzzyScale, *physics.Point) {
+	distanceToShoot := DistanceForShooting(player)
+	if distanceToShoot >= DistanceFar {
+		return MustNot, nil
+	}
+	betterTargetShoot := FindBestPointShootTheBall(player, gameMsg.GameInfo)
+	shootVector, err := physics.NewVector(player.Coords, betterTargetShoot)
+	targetAlternative := player.OpponentGoal().Center //we hope it never happens, but we need to return a value error if it does
+	if err != nil {
+		return MustNot, &targetAlternative
+	}
+	obstaclesToTarget, err := strategy.WatchOpponentOnMyRoute(player.Coords, shootVector.TargetFrom(player.Coords), units.BallSize, player.GetOpponentTeam(gameMsg.GameInfo))
+	if err != nil {
+		return MustNot, &targetAlternative
+	}
+
+	if len(obstaclesToTarget) == 0 {
+		if distanceToShoot <= DistanceBeside {
+			return Must, &betterTargetShoot
 		}
-		speed, target := d.FindBestPointShootTheBall()
-		return "Shoot!", []orders.Order{d.CreateKickOrder(target, speed)}
-	} else {
-		nextSteps := physics.NewVector(d.Player.Coords, d.OpponentGoal().Center).SetLength(units.PlayerMaxSpeed * 5)
-		obstacles := watchOpponentOnMyRoute(d.LastMsg.GameInfo, d.Player, nextSteps.TargetFrom(d.Player.Coords))
-		if len(obstacles) == 0 {
-			if MyRule == strategy.DefensePlayer && (TeamState == strategy.Neutral || TeamState == strategy.Offensive) {
-				return "Let's pass", d.orderPassTheBall()
+		if distanceToShoot <= DistanceNear {
+			return Should, &betterTargetShoot
+		}
+		if distanceToShoot <= DistanceFar {
+			return May, &betterTargetShoot
+		}
+		return ShouldNot, &betterTargetShoot
+	}
+	return MustNot, &betterTargetShoot
+}
+
+func fuzzyDecisionPass(player *client.Player, gameMsg *client.GameMessage) (FuzzyScale, []*client.Player) {
+	candidates := []*client.Player{}
+
+	decisionPass := MustNot
+	smallestDistance := float64(units.FieldWidth)
+	gameMsg.ForEachPlayByTeam(player.TeamPlace, func(index int, playerMate *client.Player) {
+		targetToPlayerMate, err := physics.NewVector(player.Coords, playerMate.Coords)
+		if err != nil {
+			return
+		}
+		distanceFromMe := player.Coords.DistanceTo(playerMate.Coords)
+		obstaclesToPlayer, err := strategy.WatchOpponentOnMyRoute(player.Coords, targetToPlayerMate.TargetFrom(player.Coords), units.BallSize, player.GetOpponentTeam(gameMsg.GameInfo))
+		if err != nil || len(obstaclesToPlayer) > 0 {
+			return //no decision bases on this player mate
+		}
+
+		shouldShoot, _ := ShouldShoot(playerMate, gameMsg)
+		if shouldShoot == Must || shouldShoot == Should {
+			if distanceFromMe >= DistanceFar {
+				return //he is at a nice position, but the ball
 			}
-			return "I am free yet", []orders.Order{d.orderAdvance()}
-		} else {
-			return "I need help guys!", d.orderPassTheBall()
+			decisionPass = Should //we are able to pass to someone who may score, so we should pass
+		}
+		smallestDistance = math.Min(distanceFromMe, smallestDistance)
+		candidates = append(candidates, playerMate)
+	})
+
+	if len(candidates) == 0 { //if I pass the ball I know I will lose it
+		return MustNot, candidates
+	}
+
+	gameMsg.ForEachPlayByTeam(player.GetOpponentPlace(), func(index int, opponent *client.Player) {
+		if opponent.Coords.DistanceTo(player.Coords) < units.PlayerSize*2 {
+			decisionPass = Must
+		}
+	})
+
+	if decisionPass < May { //we have candidates to receive the pass, but no one may score, so may pass if we decide to
+		decisionPass = ShouldNot
+		if smallestDistance <= DistanceNear {
+			decisionPass = May
 		}
 	}
+
+	return decisionPass, candidates
 }
 
 func DistanceForShooting(player *client.Player) float64 {
 	goal := player.OpponentGoal()
-	distX := math.Abs(float64(goal.Center.PosX - player.Coords.PosX))
-
-	goalCoord := physics.Point{
+	ref := physics.Point{
 		PosX: goal.Center.PosX,
 		PosY: player.Coords.PosY,
 	}
 	if player.Coords.PosY < units.GoalMinY {
-		goalCoord.PosY = goal.BottomPole.PosY
+		ref = goal.BottomPole
 	} else if player.Coords.PosY > units.GoalMaxY {
-		goalCoord.PosY = goal.TopPole.PosY
+		ref = goal.TopPole
 	}
-	return player.Coords.DistanceTo(goalCoord)
+	return player.Coords.DistanceTo(ref)
 }
 
+// FindBestPointShootTheBall find a good target in the opponent goal to shoot the ball at.
+// @todo needs enhancement: the method is only choosing a side of the goal, but could consider the player position
+func FindBestPointShootTheBall(player *client.Player, gameInfo client.GameInfo) (target physics.Point) {
+	goalkeeper := player.FindOpponentPlayer(gameInfo, arena.GoalkeeperNumber)
+	goal := player.OpponentGoal()
+	if goalkeeper.Coords.PosY > goal.Center.PosY {
+		return physics.Point{
+			PosX: goal.BottomPole.PosX,
+			PosY: goal.BottomPole.PosY + units.BallSize,
+		}
+	} else {
+		return physics.Point{
+			PosX: goal.TopPole.PosX,
+			PosY: goal.TopPole.PosY - units.BallSize,
+		}
+	}
+}
 
-fazer methodo que recebe jogador e diz se ele pode chutar
+func electBestCandidate(players []*client.Player) *client.Player {
+	return players[0]
+}
--- dummie/fuzzy.go
+++ dummie/fuzzy.go
@@ -2,17 +2,14 @@ package dummie
 
 import "github.com/makeitplay/arena/units"
 
+type FuzzyScale int
+
 const (
-	// DisputingMust means: the player MUST disput the ball
-	DisputingMust = iota
-	// DisputingShould means: the player SHOULD disput the ball if it can
-	DisputingShould
-	// DisputingMay means: the player MAY disput the ball if it want
-	DisputingMay
-	// DisputingShouldNot means: the player SHOULD not disput, but it's ok if it does
-	DisputingShouldNot
-	// DisputingMustNot means: the player MUST not disput because it should let someone elso do
-	DisputingMustNot
+	MustNot FuzzyScale = iota
+	ShouldNot
+	May
+	Should
+	Must
 )
 
 const (
