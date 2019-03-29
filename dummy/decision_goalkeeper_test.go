68b42b91df5cb693e1c105bb9e9f6de6f1812619
Rubens Silva <rubens21@gmail.com>
2019-03-29 08:24:46 -0400

    working on integration tests

--- dummy/decision_goalkeeper_test.go
+++ dummy/decision_goalkeeper_test.go
@@ -1,6 +1,7 @@
 package dummy
 
 import (
+	"context"
 	"github.com/makeitplay/arena"
 	"github.com/makeitplay/arena/physics"
 	"github.com/makeitplay/arena/units"
@@ -9,9 +10,42 @@ import (
 	"github.com/makeitplay/the-dummies-go/strategy"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"math/rand"
+	"os"
 	"testing"
 )
+
+var Controller client.Controller
+
+func TestMain(m *testing.M) {
+	ctx, stop := context.WithCancel(context.Background())
+
+	defer stop()
+	serverConfig := new(client.Configuration)
+	serverConfig.WSPort = "8080"
+	serverConfig.WSHost = "localhost"
+	serverConfig.UUID = "local"
+	integrationCtx, ctrl, err := client.NewTestController(ctx, *serverConfig)
+	if err == nil {
+		Controller = ctrl
+		//Controller.SetFrameInterval(500 * time.Millisecond)
+		go func() {
+			select {
+			case <-integrationCtx.Done():
+				log.Fatal("integration test was interrupted by the controller")
+			}
+		}()
+	}
+	// call flag.Parse() here if TestMain uses flags
+	os.Exit(m.Run())
+}
+
+func resetIntagrationState() {
+	if Controller != nil {
+		Controller.ResetScore()
+	}
+}
 
 func TestNearestGoalPoint_InFrondOfTheGoal(t *testing.T) {
 	goal := arena.HomeTeamGoal
@@ -141,8 +175,6 @@ func TestFindThreatenedSpot_BestPosition_TopPoleNoRush(t *testing.T) {
 	}
 	gamer.Play(GetInitialRegion().Center(serverConfig.TeamPlace), serverConfig)
 
-	ctrl := client.NewTestController()
-
 	//goal := arena.HomeTeamGoal
 	//
 	//ball := client.Ball{}
@@ -161,3 +193,103 @@ func TestFindThreatenedSpot_BestPosition_TopPoleNoRush(t *testing.T) {
 	//assert.True(t, coming)
 	//assert.Equal(t, expectedTarget, target)
 }
+
+func TestIntegration_KeeperShouldCatch_atCenterFromCenter(t *testing.T) {
+	if Controller == nil {
+		t.Skip("no integration available")
+	}
+
+	ballInitialPosition := arena.HomeTeamGoal.Center
+	ballInitialPosition.PosX += Units.GoalZoneRange + 1 //avoiding the ball be kicked out after "BallTimeInGoalZone" frames
+	ballVelocity := physics.NewZeroedVelocity(physics.West)
+	ballVelocity.Speed = units.BallMaxSpeed
+
+	if _, err := Controller.SetPlayerPos(arena.HomeTeam, arena.GoalkeeperNumber, arena.HomeTeamGoal.Center); err != nil {
+		t.Fatal(err)
+	}
+	gameStat, err := Controller.SetBallProperties(ballVelocity, ballInitialPosition)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ballState := gameStat.Ball()
+
+	for ballState.Velocity.Speed > 0 {
+		turnCtx, err := Controller.GetGamerCtx(arena.HomeTeam, arena.GoalkeeperNumber)
+		if err != nil {
+			t.Fatal(err)
+		}
+		player := &Dummy{
+			GameMsg: turnCtx.GameMsg(),
+			Player:  turnCtx.Player(),
+		}
+
+		_, orderList := player.orderForGoalkeeper()
+		Controller.SendOrders(arena.HomeTeam, arena.GoalkeeperNumber, orderList)
+		newState, _ := Controller.NextTurn()
+		ballState = newState.Ball()
+	}
+	if ballState.Holder == nil {
+		t.Fatal("should had caught the ball. Middle")
+	}
+	assert.Equal(t, ballState.Holder.TeamPlace, arena.HomeTeam)
+	assert.Equal(t, ballState.Holder.Number, arena.GoalkeeperNumber)
+}
+
+func TestIntegration_KeeperShouldCatch_atTopFromCenter(t *testing.T) {
+
+	ballInitialPosition := arena.HomeTeamGoal.Center
+	ballInitialPosition.PosX += Units.GoalZoneRange + Units.BallSize //avoiding the ball be kicked out after "BallTimeInGoalZone" frames
+	ballVelocity := physics.NewZeroedVelocity(physics.West)
+	ballVelocity.Speed = units.BallMaxSpeed
+
+	shootTarget := arena.HomeTeamGoal.TopPole
+	shootTarget.PosY -= 1
+	shootTop, _ := physics.NewVector(ballInitialPosition, shootTarget)
+	ballVelocity = physics.NewZeroedVelocity(*shootTop)
+	ballVelocity.Speed = units.BallMaxSpeed
+
+	gameStat, err := Controller.SetBallProperties(ballVelocity, ballInitialPosition)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ballState := gameStat.Ball()
+
+	for ballState.Velocity.Speed > 0 {
+		turnCtx, err := Controller.GetGamerCtx(arena.HomeTeam, arena.GoalkeeperNumber)
+		if err != nil {
+			t.Fatal(err)
+		}
+		player := &Dummy{
+			GameMsg: turnCtx.GameMsg(),
+			Player:  turnCtx.Player(),
+		}
+
+		_, orderList := player.orderForGoalkeeper()
+		Controller.SendOrders(arena.HomeTeam, arena.GoalkeeperNumber, orderList)
+		newState, _ := Controller.NextTurn()
+		ballState = newState.Ball()
+	}
+	if ballState.Holder == nil {
+		t.Fatal("should had caught the ball. Top")
+	}
+
+	assert.Equal(t, ballState.Holder.TeamPlace, arena.HomeTeam)
+	assert.Equal(t, ballState.Holder.Number, arena.GoalkeeperNumber)
+}
+
+//ctrl.ResetScore()
+//ctrl.SendOrders(arena.HomeTeam, arena.GoalkeeperNumber, []orders.Order{
+//
+//})
+//
+//for gameStat.Ball().Velocity.Speed > 0 {
+//	ctrl.ResetScore()
+//	gameStat, err = ctrl.NextTurn()
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	v := gameStat.Ball().Velocity
+//	logrus.Infof("Ball velocity %v", &v)
+//	ctrl.SetGameTurn( 100)
+//}
+//}
