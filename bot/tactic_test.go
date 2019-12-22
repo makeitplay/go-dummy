9b5fb1702a04980cb6b8fc017af1d3df2f6b0ac1
Rubens Silva <rubens21@gmail.com>
2019-12-22 17:48:53 -0500

    defining positions

--- bot/tactic_test.go
+++ bot/tactic_test.go
@@ -8,6 +8,15 @@ import (
 	"testing"
 )
 
+var knownTeamStates = []TeamState{
+	Initial,
+	UnderPressure,
+	Defensive,
+	Neutral,
+	Offensive,
+	OnAttack,
+}
+
 // Note that this method only tests if the tactic settings are well defined. Does not test if the settings are defined
 // as you expect. Please create your own test to check your logic.
 func TestDefineRole(t *testing.T) {
@@ -44,17 +53,27 @@ func TestDefineRegionMap_ShouldMapAllPlayersInAllTeamStates(t *testing.T) {
 	for i := uint32(2); i <= 11; i++ {
 		regMap := DefineRegionMap(i)
 		assert.NotNil(t, regMap, fmt.Sprintf("missing maps for player %d", i))
-		_, ok := regMap[Initial]
-		assert.True(t, ok, fmt.Sprintf("missing %s map for player %d", Initial, i))
-		_, ok = regMap[UnderPressure]
-		assert.True(t, ok, fmt.Sprintf("missing %s map for player %d", UnderPressure, i))
-		_, ok = regMap[Defensive]
-		assert.True(t, ok, fmt.Sprintf("missing %s map for player %d", Defensive, i))
-		_, ok = regMap[Neutral]
-		assert.True(t, ok, fmt.Sprintf("missing %s map for player %d", Neutral, i))
-		_, ok = regMap[Offensive]
-		assert.True(t, ok, fmt.Sprintf("missing %s map for player %d", Offensive, i))
-		_, ok = regMap[OnAttack]
-		assert.True(t, ok, fmt.Sprintf("missing %s map for player %d", OnAttack, i))
+		for _, state := range knownTeamStates {
+			_, ok := regMap[state]
+			assert.True(t, ok, fmt.Sprintf("missing %s map for player %d", state, i))
+		}
+	}
+}
+
+func TestDefineRegionMap_AllMappedRegionShouldBeCompatibleWithOurPositioner(t *testing.T) {
+	p, err := coach.NewPositioner(RegionCols, RegionRows, proto.Team_HOME)
+	if err != nil {
+		t.Fatalf("invalid settings, cannot created a NewPositioner: %s", err)
+	}
+
+	// starting from 2 because the number goalkeeper has RegionMap
+	for i := uint32(2); i <= 11; i++ {
+		regMap := DefineRegionMap(i)
+		assert.NotNil(t, regMap, fmt.Sprintf("missing maps for player %d", i))
+		for _, state := range knownTeamStates {
+			r := regMap[state]
+			_, err = p.GetRegion(r.Col, r.Row)
+			assert.Nil(t, err, fmt.Sprintf("invalid region mapped for state %s player %d: %v", state, i, r))
+		}
 	}
 }
